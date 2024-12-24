655a66861f7fb22580f7fd94b35961e42c878402
Sunchhay <sunchhay395@gmail.com>
2024-12-25 03:53:05 +0700

    add some target layer mask for trash and removed idle animation

--- Assets/Scripts/Controllers/RayCast.cs
+++ Assets/Scripts/Controllers/RayCast.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class RayCast : MonoBehaviour
 {
+    [SerializeField] LayerMask targetLayerMask;
     RaycastHit hitInfo;
     // Update is called once per frame
     void Update()
     {
         Ray ray = new Ray(transform.position, transform.TransformDirection(Vector3.forward));
 
-        if (Physics.Raycast(ray, out hitInfo, 20f))
+        if (Physics.Raycast(ray, out hitInfo, 20f, targetLayerMask))
         {
             Debug.Log("Hit something");
             Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hitInfo.distance, Color.red);
