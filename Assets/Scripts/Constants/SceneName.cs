74b0ec1714f92be427cc6b7b9374ecaabc91b972
Sunchhay <sunchhay395@gmail.com>
2025-01-07 14:21:58 +0700

    chore: re-organize assets

---
+++ Assets/Scripts/Constants/SceneName.cs
@@ -0,0 +1,10 @@
+class SceneNameEnum
+{
+  public const string SORTING_SCENE = "SortingScene";
+  public const string START_SCENE = "StartScene";
+  public const string APPEARANCE_SCENE = "AppearanceScene";
+  public const string LEVEL_SCENE = "LevelScene";
+  public const string LEVEL_1 = "Level1Scene";
+  public const string LEVEL_3 = "Level3Scene";
+  public const string LEVEL_2 = "Level2Scene";
+}
